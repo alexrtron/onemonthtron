e5fa6ec7c08c3b3c20d8657ff4c3220a68d5f4a5
alexrtron <alexrtron@gmail.com>
2015-10-10 23:41:29 +0000

    Implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+  
+  # Support stripe payments thru charges
+  resources :charges
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
